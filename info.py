a28419f9ac2ffeb61b312ca6d2044853ced4e458
rajdev1009 <mrdev4772@gmail.com>
2025-12-23 00:04:25 +0530

    Create info.py

---
+++ info.py
@@ -0,0 +1,15 @@
+# Yahan apni images aur unka text dalein
+PHOTO_GALLERY = [
+    {"id": 1, "url": "https://link-to-your-photo1.jpg", "text": "Pehli Photo Ka Caption"},
+    {"id": 2, "url": "https://link-to-your-photo2.jpg", "text": "Doosri Photo Ka Caption"},
+    {"id": 3, "url": "https://link-to-your-photo3.jpg", "text": "Teesri Photo Ka Caption"},
+    # ... isi tarah 20 tak copy-paste karein
+    {"id": 20, "url": "https://link-to-your-photo20.jpg", "text": "Beesvin Photo Ka Caption"},
+]
+
+# Baaki settings
+BOT_CONFIG = {
+    "NAME": "Raj's Assistant",
+    "AVATAR": "https://cdn-icons-png.flaticon.com/512/4712/4712027.png",
+    "NEWS_LINK": "https://news.google.com"
+}
