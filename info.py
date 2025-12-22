8894f965a7cd6eeb3705f812be8308354c47ed16
rajdev1009 <mrdev4772@gmail.com>
2025-12-23 00:54:57 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,14 +1,11 @@
-# --- info.py ---
-
-# Aapka sara personal data yahan hai
 BOT_CONFIG = {
     "NAME": "Raj's Assistant",
     "AVATAR": "https://cdn-icons-png.flaticon.com/512/4712/4712027.png", 
     "NEWS_LINK": "https://news.google.com",
-    "TELEGRAM_LINK": "https://t.me/YourTelegramChannel" # Apna link yahan badlein
+    "TELEGRAM_LINK": "https://t.me/YourChannelLink" # <-- Apna link yahan dalein
 }
 
-# 20 Alag-alag Default Photos ki list
+# 20 Different Images
 PHOTO_GALLERY = [
     {"id": 1, "url": "https://picsum.photos/id/10/600/400", "text": "Nature View - Photo 1"},
     {"id": 2, "url": "https://picsum.photos/id/20/600/400", "text": "Tech Setup - Photo 2"},
@@ -23,7 +20,7 @@ PHOTO_GALLERY = [
     {"id": 11, "url": "https://picsum.photos/id/57/600/400", "text": "Winter Snow - Photo 11"},
     {"id": 12, "url": "https://picsum.photos/id/60/600/400", "text": "Foggy Morning - Photo 12"},
     {"id": 13, "url": "https://picsum.photos/id/64/600/400", "text": "Golden Sunset - Photo 13"},
-    {"id": 14, "url": "https://picsum.photos/id/75/600/400", "text": "Vegetable Garden - Photo 14"},
+    {"id": 14, "url": "https://picsum.photos/id/75/600/400", "text": "Agriculture - Photo 14"},
     {"id": 15, "url": "https://picsum.photos/id/82/600/400", "text": "Flower Bloom - Photo 15"},
     {"id": 16, "url": "https://picsum.photos/id/91/600/400", "text": "Abstract Art - Photo 16"},
     {"id": 17, "url": "https://picsum.photos/id/102/600/400", "text": "Wild Animal - Photo 17"},
