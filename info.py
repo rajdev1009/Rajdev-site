99dc1bbc26bb6880cfd5568dfe60707a57875557
rajdev1009 <mrdev4772@gmail.com>
2025-12-23 18:01:48 +0530

    Update info.py

--- info.py
+++ info.py
@@ -2,9 +2,9 @@
 BOT_CONFIG = {
     "NAME": "Dev (Raj's Assistant)",
     # niche apni normal photo ka link dalein (Munh band)
-    "AVATAR_NORMAL": "https://i.ibb.co/example/normal-face.png", 
+    "AVATAR_NORMAL": "https://i.ibb.co/kY1Nyzs/1765464889401-2.jpg", 
     # niche wahi photo dalein jisme munh thoda khula ho
-    "AVATAR_TALKING": "https://i.ibb.co/example/talking-face.png",
+    "AVATAR_TALKING": "https://i.ibb.co/bMDGJ9KK/1766492848052-2.jpg",
     
     "SMARTLINK_URL": "https://www.effectivegatecpm.com/gkz2ad9kk?key=e207a1458b4f261bfa34724b55f98bf3" 
 }
